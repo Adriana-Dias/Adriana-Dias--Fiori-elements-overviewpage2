3bb9b9751f8a0cf893519e1b82f04c8e124ed31b
Adriana Dias <adias.adriana@gmail.com>
2023-03-27 17:15:02 -0300

    versão inicial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("devfiori50.indicadorrecrutamento.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
